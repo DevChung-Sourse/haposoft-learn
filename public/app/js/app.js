93f54431bc9a130bb333d9f86813fd3182b6e94c
NGUYENCHUNG <chungm197@gmail.com>
2022-03-15 09:13:54 +0700

    #12 commit to create feedback, startLearn, statistic

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -2,5 +2,30 @@ const navBarIcon = document.querySelector(".nav-bar-icon")
 const menu = document.querySelector("#menu")
 
 navBarIcon.addEventListener("click", () => {
-    menu.classList.toggle("active-menu")
+  menu.classList.toggle("active-menu")
+})
+
+$(document).ready(() => {
+  $('.feedback-carousel').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrows: false,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+  })
 })
