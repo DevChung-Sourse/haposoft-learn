e7dd3fa896521724465b14bbf3a0d110cacc3f70
NGUYENCHUNG <chungm197@gmail.com>
2022-03-18 14:19:13 +0700

    #15 commit to create footer, login, register

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -26,6 +26,31 @@ $(document).ready(() => {
           slidesToShow: 1,
           slidesToScroll: 1
         }
-      }]
+      }
+    ]
+  })
+
+  const login = $('#login')
+  const register = $('#register')
+  
+  const formLogin = $('#formLogin')
+  const formRegister = $('#formRegister')
+
+  formRegister.click ( ()=> {
+    if (!formRegister.hasClass('active-lr') && !register.hasClass('active-display')) {
+      formRegister.addClass('active-lr')
+      register.addClass('active-display')
+      formLogin.removeClass('active-lr')
+      login.removeClass('active-display')
+    }
+  })
+
+  formLogin.click ( ()=> {
+    if (!formLogin.hasClass('active-lr') && !login.hasClass('active-display')) {
+      formLogin.addClass('active-lr')
+      login.addClass('active-display')
+      formRegister.removeClass('active-lr')
+      register.removeClass('active-display')
+    }
   })
 })
