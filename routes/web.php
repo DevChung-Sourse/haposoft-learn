d0e0c221cd809f5d1d46d446c698430dfd5576e3
NGUYENCHUNG <chungm197@gmail.com>
2022-04-05 13:12:29 +0700

    #23 create view blade with hapolearn

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/h', function () {
+    return view('home');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('public/app/js/app.js', 'public/app/public/js')
+mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('public/app/scss/app.scss', 'public/app/public/css');
+    .sass('resources/sass/app.scss', 'public/css');
